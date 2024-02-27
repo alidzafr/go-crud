d65262d79bf913bebd17d69020000c8a6a7e44b3
alidzafr <alidzafathur@gmail.com>
2024-02-27 09:10:43 +0700

    Crete routing & controller

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fiberGorm/controllers/bookcontroller"
 	"fiberGorm/models"
 
 	"github.com/gofiber/fiber/v2"
@@ -14,6 +15,14 @@ func main() {
 	// Initial Route (dinonaktifkan karena agak ribet)
 	// Rename handler into controllers
 	// route.RouteInit(app)
+
+	apiBook := app.Group("/api/books")
+
+	apiBook.Get("/", bookcontroller.Index)
+	apiBook.Get("/:id", bookcontroller.Show)
+	apiBook.Post("/", bookcontroller.Create)
+	apiBook.Put("/:id", bookcontroller.Update)
+	apiBook.Delete("/:id", bookcontroller.Delete)
 
 	app.Listen(":8000")
 }
